7fa56406974a6070276ecb463f6c9c3f795d80ce
abhijithb200 <abhijithb007m@gmail.com>
2023-03-16 10:49:38 +0530

    added code guardian

--- Application/app/go.mod
+++ Application/app/go.mod
@@ -2,8 +2,9 @@ module app
 
 go 1.20
 
+require github.com/z7zmey/php-parser v0.7.2
+
 require (
 	github.com/pkg/profile v1.4.0 // indirect
 	github.com/yookoala/realpath v1.0.0 // indirect
-	github.com/z7zmey/php-parser v0.7.2 // indirect
 )
--- Application/app/main.go
+++ Application/app/main.go
@@ -1,31 +1,20 @@
 package main
 
-import (
-	"bytes"
-	"fmt"
-	"os"
-
-	"github.com/z7zmey/php-parser/php5"
-	"github.com/z7zmey/php-parser/visitor"
-)
+import "app/parser"
 
 func main() {
-	src := bytes.NewBufferString(`<? echo "Hello world";`)
+	// src := []byte(`<?php
+	// echo 'my name is'.$_GET['name'];
+	// `)
 
-	parser := php5.NewParser(src, "example.php")
-	parser.Parse()
+	// parser := php5.NewParser(src, "example.php")
+	// parser.Parse()
 
-	for _, e := range parser.GetErrors() {
-		fmt.Println(e)
-	}
-
-	visitor := visitor.Dumper{
-		Writer:    os.Stdout,
-		Indent:    "",
-		Comments:  parser.GetComments(),
-		Positions: parser.GetPositions(),
-	}
-
-	rootNode := parser.GetRootNode()
-	rootNode.Walk(visitor)
+	// visitor := visitor.GoDumper{
+	// 	Writer: os.Stdout,
+	// }
+	// //
+	// rootNode := parser.GetRootNode()
+	// rootNode.Walk(&visitor)
+	parser.Parser()
 }
